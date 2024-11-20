b1ade68e039ced07d04b03ff34d668c317f4a007
NCDinesh <deenez.npl1@gmail.com>
2024-11-20 22:52:28 +0545

    Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,28 @@
 import React from "react";
-import "Navbar.css";
+import "./Navbar.css";
 
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <nav>
+      <a href="#" className="logo">
+        Sound<span className="red">Design</span>
+      </a>
+      <ul className="nav-links">
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Course Details</a>
+        </li>
+        <li>
+          <a href="#">Blog</a>
+        </li>
+        <li>
+          <a href="#">Testimonial</a>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
