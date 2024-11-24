3b2ca19de3404f85b1385be35126d478c2f623ba
NCDinesh <deenez.npl1@gmail.com>
2024-11-24 09:06:25 +0545

    Info section completed

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -1,8 +1,31 @@
 import React from "react";
 import "./Info.css";
+import studenticon from "../../assets/student-icon.svg";
+import videoicon from "../../assets/video-icon.svg";
+import group2 from "../../assets/Group2.svg";
 
 const Info = () => {
-  return <div>Info</div>;
+  return (
+    <section id="info" className="dark-gray">
+      <div className="wrapper">
+        <div className="content-container">
+          <div className="info-content">
+            <img src={studenticon} alt="" />
+            <div className="amount">23,000+</div>
+            <div className="type">Students</div>
+          </div>
+
+          <div className="info-content">
+            <img src={videoicon} alt="" />
+            <div className="amount">26 Hrs</div>
+            <div className="type">Video Content</div>
+          </div>
+        </div>
+
+        <img src={group2} className="bg-element-2" alt="" />
+      </div>
+    </section>
+  );
 };
 
 export default Info;
