eb9adb5ad67c26a6af42fa96f31390f2ab3a38b6
NCDinesh <deenez.npl1@gmail.com>
2024-11-25 23:38:46 +0545

    Ongoing responsiveness

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,27 +1,56 @@
 import React from "react";
 import "./Navbar.css";
+import menuicon from "../../assets/menuicon.svg";
+import closeicon from "../../assets/closeicon.png";
 
 const Navbar = () => {
   return (
-    <nav>
-      <a href="#" className="logo">
-        Sound<span className="red">Design</span>
-      </a>
-      <ul className="nav-links">
-        <li>
-          <a href="#">About</a>
-        </li>
-        <li>
-          <a href="#">Course Details</a>
-        </li>
-        <li>
-          <a href="#">Blog</a>
-        </li>
-        <li>
-          <a href="#">Testimonial</a>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <nav>
+        <a href="#" className="logo">
+          Sound<span className="red">Design</span>
+        </a>
+        <ul className="nav-links">
+          <li>
+            <a href="#info">About</a>
+          </li>
+          <li>
+            <a href="#topics">Course Details</a>
+          </li>
+          <li>
+            <a href="#blog">Blog</a>
+          </li>
+          <li>
+            <a href="#testimonials">Testimonial</a>
+          </li>
+        </ul>
+
+        <div className="menu-icon">
+          <img src={menuicon} alt="" />
+        </div>
+      </nav>
+
+      <div className="mobile-menu-container">
+        <div className="close-icon">
+          <img src={closeicon} alt="" />
+        </div>
+
+        <ul className="menu-items">
+          <li>
+            <a href="#info">About</a>
+          </li>
+          <li>
+            <a href="#topics">Course Details</a>
+          </li>
+          <li>
+            <a href="#blog">Blog</a>
+          </li>
+          <li>
+            <a href="#testimonials">Testimonial</a>
+          </li>
+        </ul>
+      </div>
+    </>
   );
 };
 
